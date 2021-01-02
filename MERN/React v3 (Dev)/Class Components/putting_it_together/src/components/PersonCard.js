04191cfa8a7233c7172fce292fc2ed07ec21632b
ethan-mace <ethanmace@protonmail.com>
2021-01-02 15:36:14 -0600

    Putting it Together project complete

---
+++ MERN/React v3 (Dev)/Class Components/putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component{
+    person = {
+        firstName: this.props.firstName,
+        lastName: this.props.lastName,
+        age: this.props.age,
+        hairColor: this.props.hairColor,
+        id: this.props.firstName + this.props.lastName + this.props.hairColor
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.person.lastName}, {this.person.firstName}</h1>
+                <p>Age: <span>{this.person.age}</span></p>
+                <p>Hair Color: {this.person.hairColor}</p>
+                <button onClick={this.increment}>Birthday Button for {this.person.firstName} {this.person.lastName}</button>
+            </div>
+        );
+    }
+    increment = () => {
+        this.setState({person: this.person.age = this.person.age + 1});
+    }
+}
+export default PersonCard;
--- MERN/React v3 (Dev)/Intro to React/putting_it_together/src/components/IncrementAge.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class IncrementAge extends Component{
-    render(){
-        return(
-            <button onClick={this.ag}>Birthday Button for {this.props.name}</button>
-        );
-    }
-}
