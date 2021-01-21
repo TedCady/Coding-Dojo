b972bcebfa21272d25b6a8d75415e00efecd3eac
ethan-mace <ethanmace@protonmail.com>
2021-01-21 16:30:31 -0600

    Jokes API complete

--- MERN/React Mongo/MongoDB/Jokes API/server/controllers/jokes.controller.js
+++ MERN/React Mongo/MongoDB/Jokes API/server/controllers/jokes.controller.js
@@ -1,31 +1,33 @@
 const Joke = require('../models/jokes.model');
 
+let errLoc = 1
+
 module.exports.findAllJokes = (req, res) => {
     Joke.find()
         .then(results => res.json({jokes: results}))
-        .catch(err => res.json({message: 'Error:', error: err}))
+        .catch(err => res.json({message: `Error in .controllers location: ${errLoc}`, error: err}))
 }
-
 module.exports.findOneJoke = (req, res) => {
+    errLoc = 2;
     Joke.findOne({_id: req.params.id})
         .then(results => res.json({joke: results}))
-        .catch(err => res.json({message: 'Error:', error: err}))
+        .catch(err => res.json({message: `Error in .controllers location: ${errLoc}`, error: err}))
 }
-
 module.exports.createJoke = (req, res) => {
+    errLoc = 3;
     Joke.create(req.body)
         .then(results => res.json({joke: results}))
-        .catch(err => res.json({message: 'Error:', error: err}))
+        .catch(err => res.json({message: `Error in .controllers location: ${errLoc}`, error: err}))
 }
-
 module.exports.updateJoke = (req, res) => {
-    Joke.create({_id: req.params.id}, req.body, {new:true})
+    errLoc = 4;
+    Joke.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
         .then(results => res.json({joke: results}))
-        .catch(err => res.json({message: 'Error:', error: err}))
+        .catch(err => res.json({message: `Error in .controllers location: ${errLoc}`, error: err}))
 }
-
 module.exports.deleteJoke = (req, res) => {
+    errLoc = 5;
     Joke.deleteOne({_id: req.params.id})
         .then(results => res.json({joke: results}))
-        .catch(err => res.json({message: 'Error:', error: err}))
+        .catch(err => res.json({message: `Error in .controllers location: ${errLoc}`, error: err}))
 }
--- MERN/React Mongo/MongoDB/Jokes API/server/server.js
+++ MERN/React Mongo/MongoDB/Jokes API/server/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const app = express();
 
+require('../server/config/mongoose.config');
+
+app.use(express.json(), express.urlencoded({extended:true}));
+
+const Routes = require('../server/routes/jokes.routes');
+Routes(app);
+
+app.listen(8000, () => console.log('Connection to port 8000 established'))
