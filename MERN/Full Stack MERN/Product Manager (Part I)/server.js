ccc0924049987b0c739138850c647f17b7d8809d
ethan-mace <ethanmace@protonmail.com>
2021-01-22 12:45:12 -0600

    updates

--- MERN/Full Stack MERN/Product Manager (Part I)/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('./server/config/mongoose.config'); // This is new
-app.use(cors(), express.json(), express.urlencoded({ extended: true }));
-require('./server/routes/person.routes')(app);
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
--- MERN/Full Stack MERN/Product Manager (Part I)/server/models/product.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = new mongoose.Schema({
-    firstName: { type: String },
-    lastName: { type: String }
-}, { timestamps: true });
-module.exports.Product = mongoose.model('Product', ProductSchema);
