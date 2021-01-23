79f16b04b8301b2559a2ce697357efe4c5a9060f
ethan-mace <ethanmace@protonmail.com>
2021-01-23 14:17:14 -0600

    Project Manager complete - consolidated

--- MERN/Full Stack MERN/product_manager_part_i/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-
-module.exports = app => {
-    app.get('/api/products/', ProductController.findAll);
-    app.get('/api/products/:id', ProductController.findOne);
-    app.put('/api/products/update/:id', ProductController.updateOne);
-    app.post('/api/products/new/', ProductController.createNew);
-    app.delete('/api/products/delete/:id', ProductController.deleteItem);
-}
