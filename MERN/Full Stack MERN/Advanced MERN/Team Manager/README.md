a1c7c88e9f0e5a5ebcafde733d5acaebec071270
ethan-mace <ethanmace@protonmail.com>
2021-01-26 09:48:17 -0600

    moved belt_exam to private

--- MERN/Full Stack MERN/Advanced MERN/Team Manager/README.md
+++
@@ -1,3 +0,0 @@
-# Team Manager
-[![img](static/preview.jpg)](https://youtu.be/6hmjlv1eAT4)
-## Objectives: 
--- MERN/Full Stack MERN/Advanced MERN/Team Manager/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/app',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('server/config/mongoose.config: connection to database established'))
-    .catch(err => console.log('server.config.mongoose.config: ' + err))
